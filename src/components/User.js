57b28997087da601db7173ee2ff2e63f873d1563
joaonacho <joaonacho@gmail.com>
2022-06-17 22:02:49 +0100

    user component WIP

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,26 @@
 import React from "react";
 
 export const User = () => {
-  return <div>User</div>;
+  return (
+    <section
+      style={{
+        backgroundColor: "whitesmoke",
+        height: "100vh",
+        marginLeft: "300px",
+        width: "1187px",
+        position: "absolute",
+      }}
+    >
+      <div
+        style={{
+          textAlign: "left",
+          marginTop: "50px",
+          marginLeft: "50px",
+          width: "120px",
+        }}
+      >
+        <h1>Utilizador</h1>
+      </div>
+    </section>
+  );
 };
